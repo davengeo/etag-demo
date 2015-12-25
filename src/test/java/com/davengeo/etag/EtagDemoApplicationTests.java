33140975832926373664535e8c1ce6d2ff979664
David Navarro Alvarez <davengeo@yahoo.es>
2015-12-25 18:50:01 +0100

    with the right behaviour

--- src/test/java/com/davengeo/etag/EtagDemoApplicationTests.java
+++ src/test/java/com/davengeo/etag/EtagDemoApplicationTests.java
@@ -1,25 +1,25 @@
 package com.davengeo.etag;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-
 
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
+@SuppressWarnings("SpringJavaAutowiredMembersInspection")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = EtagDemoApplication.class)
 @WebAppConfiguration
@@ -40,11 +40,17 @@ public class EtagDemoApplicationTests {
     }
 
 	@Test
-	public void contextLoads() throws Exception {
-        mockMvc.perform(get("/check").
+	public void should_be_200_or_304_with_IfNoneMatch_Header() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/check").
                 accept("application/hal+json")).
-                andExpect(header().string("ETag", instanceOf(String.class)));
+                andExpect(header().string("ETag", instanceOf(String.class))).
+                andReturn();
 
+        final String etag = mvcResult.getResponse().getHeader("Etag");
+
+        mockMvc.perform(get("/check").
+                accept("application/hal+json").header("If-None-Match", etag)).
+                andExpect(MockMvcResultMatchers.status().isNotModified());
 	}
 
 }
