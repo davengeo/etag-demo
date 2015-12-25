e591d949bf55057087a99348397d65dd66c30491
David Navarro Alvarez <davengeo@yahoo.es>
2015-12-25 12:50:09 +0100

    Initial commit

---
+++ src/test/java/com/davengeo/etag/EtagDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.davengeo.etag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EtagDemoApplication.class)
+@WebAppConfiguration
+public class EtagDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
