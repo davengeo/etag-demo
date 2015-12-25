3e40b4727d11165b32defc801115fc69a1c713d8
David Navarro Alvarez <davengeo@yahoo.es>
2015-12-25 22:58:38 +0100

    with additive endpoint and deleteAll

--- src/main/java/com/davengeo/etag/ResourceController.java
+++ src/main/java/com/davengeo/etag/ResourceController.java
@@ -4,12 +4,15 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 public class ResourceController {
@@ -18,12 +21,12 @@ public class ResourceController {
             .maximumSize(1000)
             .build();
 
-    @RequestMapping(value="/check", method= RequestMethod.GET)
+    @RequestMapping(value="/check", method= GET)
     public Resource<String> check() {
         return new Resource<>("hello");
     }
 
-    @RequestMapping(value="/channel/{channelId}", method = RequestMethod.GET)
+    @RequestMapping(value="/channel/{channelId}", method = GET)
     public ResponseEntity<String> findOne(@PathVariable("channelId") String channelId) {
         Optional<String> present = Optional.ofNullable(cache.getIfPresent(channelId));
         if(present.isPresent()) {
@@ -33,13 +36,27 @@ public class ResourceController {
         }
     }
 
-    @RequestMapping(value="/channel/{channelId}", method = RequestMethod.POST)
+    @RequestMapping(value="/channel/{channelId}", method = POST)
     public ResponseEntity<String> createOne(@PathVariable("channelId") String channelId,
                                             @RequestBody String content) {
         cache.put(channelId, content);
         return new ResponseEntity<>(OK);
     }
 
+    @RequestMapping(value = "/channel/", method = GET)
+    public ResponseEntity<String> findAll() {
+        return new ResponseEntity<>(cache.
+                asMap().
+                keySet().
+                stream().
+                reduce("", (s, key) -> s += cache.getIfPresent(key)), OK);
+    }
+
+    @RequestMapping(value = "/channel/", method = DELETE)
+    public ResponseEntity<String> delete() {
+        cache.invalidateAll();
+        return new ResponseEntity<>(NO_CONTENT);
+    }
 
 
 }
--- src/test/java/com/davengeo/etag/EtagDemoApplicationTests.java
+++ src/test/java/com/davengeo/etag/EtagDemoApplicationTests.java
@@ -14,8 +14,7 @@ import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -28,31 +27,38 @@ public class EtagDemoApplicationTests {
     public static final String ETAG = "ETag";
     public static final String CONTENT = "content";
     public static final String NEW_CONTENT = "new content";
-    @Autowired
-    private WebApplicationContext context;
+    public static final String APPLICATION_HAL_JSON = "application/hal+json";
 
+    @Autowired
+    WebApplicationContext context;
     @Autowired
     ShallowEtagHeaderFilter shallowEtagHeaderFilter;
 
     private MockMvc mockMvc;
 
+
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(context).
-                addFilter(shallowEtagHeaderFilter, "/*").alwaysDo(print()).build();
+                addFilter(shallowEtagHeaderFilter, "/*").
+                alwaysDo(print()).
+                build();
+
+        mockMvc.perform(delete("/channel/")).
+                andExpect(status().isNoContent());
     }
 
     @Test
     public void should_be_200_or_304_with_IfNoneMatch_Header() throws Exception {
         final String etag = mockMvc.perform(get("/check").
-                accept("application/hal+json")).
+                accept(APPLICATION_HAL_JSON)).
                 andExpect(header().string(ETAG, instanceOf(String.class))).
                 andReturn().
                 getResponse().
                 getHeader(ETAG);
 
         mockMvc.perform(get("/check").
-                accept("application/hal+json").
+                accept(APPLICATION_HAL_JSON).
                 header("If-None-Match", etag)).
                 andExpect(status().isNotModified());
     }
@@ -61,16 +67,16 @@ public class EtagDemoApplicationTests {
     public void should_behave_as_etag() throws Exception {
 
         mockMvc.perform(get("/channel/1").
-                accept("application/hal+json")).
+                accept(APPLICATION_HAL_JSON)).
                 andExpect(status().isNotFound());
 
         mockMvc.perform(post("/channel/1").
-                accept("application/hal+json").
+                accept(APPLICATION_HAL_JSON).
                 content(CONTENT)).
                 andExpect(status().isOk());
 
         final String etag = mockMvc.perform(get("/channel/1").
-                accept("application/hal+json")).
+                accept(APPLICATION_HAL_JSON)).
                 andExpect(status().isOk()).
                 andExpect(header().string(ETAG, instanceOf(String.class))).
                 andExpect(content().string(equalTo(CONTENT))).
@@ -78,17 +84,17 @@ public class EtagDemoApplicationTests {
                     getResponse().getHeader(ETAG);
 
         mockMvc.perform(get("/channel/1").
-                accept("application/hal+json").
+                accept(APPLICATION_HAL_JSON).
                 header("If-None-Match", etag)).
                 andExpect(status().isNotModified());
 
         mockMvc.perform(post("/channel/1").
-                accept("application/hal+json").
+                accept(APPLICATION_HAL_JSON).
                 content(NEW_CONTENT)).
                 andExpect(status().isOk());
 
         final String etag2 = mockMvc.perform(get("/channel/1").
-                accept("application/hal+json").
+                accept(APPLICATION_HAL_JSON).
                 header("If-None-Match", etag)).
                 andExpect(status().isOk()).
                 andExpect(content().string(equalTo(NEW_CONTENT))).
@@ -96,10 +102,36 @@ public class EtagDemoApplicationTests {
                 getResponse().getHeader(ETAG);
 
         mockMvc.perform(get("/channel/1").
-                accept("application/hal+json").
+                accept(APPLICATION_HAL_JSON).
                 header("If-None-Match", etag2)).
                 andExpect(status().isNotModified());
 
+    }
+
+    @Test
+    public void should_etag_with_additive_endpoint() throws Exception {
+
+        mockMvc.perform(post("/channel/1").
+                accept(APPLICATION_HAL_JSON).
+                content("AaB")).
+                andExpect(status().isOk());
+
+        mockMvc.perform(post("/channel/2").
+                accept(APPLICATION_HAL_JSON).
+                content("BbC")).
+                andExpect(status().isOk());
+
+        final String etag = mockMvc.perform(get("/channel/").
+                accept(APPLICATION_HAL_JSON)).
+                andExpect(status().isOk()).
+                andExpect(content().string(equalTo("AaBBbC"))).
+                andReturn().
+                getResponse().getHeader(ETAG);
+
+        mockMvc.perform(get("/channel/").
+                accept(APPLICATION_HAL_JSON).
+                header("If-None-Match", etag)).
+                andExpect(status().isNotModified());
     }
 
 }
